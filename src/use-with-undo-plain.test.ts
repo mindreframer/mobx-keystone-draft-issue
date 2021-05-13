17acd7ac4d22715e1e152406611bed8f3c83fc08
Roman Heinrich <roman.heinrich@gmail.com>
2021-05-14 00:51:27 +0200

    prepare the plain version

---
+++ src/use-with-undo-plain.test.ts
@@ -0,0 +1,128 @@
+import { expect } from "@jest/globals";
+import { toJS } from "mobx";
+import { undoMiddleware } from "mobx-keystone";
+import "./commonSetup";
+import {
+  Form2,
+  Root,
+  Settings,
+  SettingsUtils,
+  Tag,
+} from "./use-with-undo-plain";
+
+const logAll = (v: any, label = "") => {
+  console.log(label, JSON.stringify(toJS(v), null, 2));
+};
+
+describe("Root", () => {
+  it("should support drafts wrapped in undo/redo middleware ", () => {
+    const root = new Root({});
+    root.setSettings(new Settings({}));
+    const settings = new Settings({
+      tags: [new Tag({ label: "tag1" }), new Tag({ label: "tag2" })],
+    });
+    root.setSettings(settings);
+    const undoManagerSettingsDraft = undoMiddleware(root.settingsDraft!.data);
+    const draftModel = root.settingsDraft!;
+    draftModel.data.arrayRemoveAt(draftModel.data.tags, 0); // this is recorded
+    expect(draftModel).toMatchSnapshot("DRAFT after direct modification");
+    undoManagerSettingsDraft.undo();
+    expect(draftModel.data.tags.length).toBe(2);
+    undoManagerSettingsDraft.redo();
+    expect(draftModel.data.tags.length).toBe(1);
+    draftModel.data.setName("HELLO");
+
+    expect(undoManagerSettingsDraft.store.undoEvents).toMatchSnapshot(
+      "Undo Events"
+    );
+    root.commit();
+    expect(draftModel).toMatchSnapshot("DRAFT AFTER COMMIT");
+
+    draftModel.data.addForm1();
+    draftModel.data.addForm1();
+    draftModel.data.addForm2();
+    draftModel.data.addForm2();
+
+    expect(undoManagerSettingsDraft.store.undoEvents).toMatchSnapshot(
+      "Undo Events with forms"
+    );
+
+    draftModel.data.arrayPush(
+      draftModel.data.form1List[0].tags,
+      new Tag({ label: "tag1" })
+    );
+    draftModel.data.arrayPush(
+      draftModel.data.form1List[0].tags,
+      new Tag({ label: "tag2" })
+    );
+
+    expect(draftModel.data.form1List[0].tags).toMatchSnapshot(
+      "tags form1 - 0 - 2 tags added"
+    );
+    undoManagerSettingsDraft.undo();
+    expect(draftModel.data.form1List[0].tags).toMatchSnapshot(
+      "tags form1 - 0 - undo -> 1 tag left"
+    );
+
+    undoManagerSettingsDraft.redo();
+
+    expect(draftModel.data.form1List[0].tags).toMatchSnapshot(
+      "tags form1 - 0 - redo -> 2 tags again"
+    );
+
+    expect(root.settings).toMatchSnapshot("settings before commit");
+    root.commit();
+    expect(root.settings).toMatchSnapshot("settings after commit");
+  });
+
+  it("should support path resolution", () => {
+    const root = new Root({});
+    const settings = new Settings({
+      tags: [new Tag({ label: "tag1" }), new Tag({ label: "tag2" })],
+    });
+    root.setSettings(settings);
+    const undoManagerSettingsDraft = undoMiddleware(root.settingsDraft!.data);
+    const draftModel = root.settingsDraft!;
+    draftModel.data.addForm1();
+    draftModel.data.addForm1();
+    draftModel.data.addForm2();
+    draftModel.data.addForm2();
+    const form2_1 = draftModel.data.form2List[0];
+
+    const form2_1_path = SettingsUtils.pathFromChild(form2_1);
+    expect(form2_1_path).toMatchSnapshot("form2_1 path");
+
+    const not_found_path = SettingsUtils.pathFromChild(root);
+    expect(not_found_path).toBeUndefined();
+  });
+
+  it("should remember selected path and restore it on settings update", () => {
+    const root = new Root({});
+    const settings = new Settings({
+      tags: [new Tag({ label: "tag1" }), new Tag({ label: "tag2" })],
+    });
+    root.setSettings(settings);
+    const draftModel = root.settingsDraft!;
+    draftModel.data.addForm1();
+    draftModel.data.addForm1();
+    draftModel.data.addForm2();
+    draftModel.data.addForm2();
+
+    const form2_1 = draftModel.data.form2List[0];
+    root.setSelectionPath(form2_1);
+    expect(root.selectionPath).toMatchSnapshot("selectionPath");
+    expect(root.selectedChild).toMatchSnapshot("selectedChild");
+
+    // prepare new settings with different data, imagine this is the response from the backend
+
+    const settings2 = new Settings({
+      tags: [new Tag({ label: "tag1" }), new Tag({ label: "tag2" })],
+    });
+    settings2.setForm2List([new Form2({ name: "changed from backend!" })]);
+
+    root.setSettings(settings2);
+
+    expect(root.selectionPath).toMatchSnapshot("selectionPath");
+    expect(root.selectedChild).toMatchSnapshot("selectedChild  - from backend");
+  });
+});
---
+++ src/use-with-undo-plain.ts
@@ -0,0 +1,136 @@
+import { observable } from "mobx";
+import {
+  Draft,
+  draft,
+  findParentPath,
+  model,
+  Model,
+  modelAction,
+  prop,
+  resolvePath,
+} from "mobx-keystone";
+
+interface ITag {
+  label: string;
+}
+
+interface IForm1 {
+  name: string;
+  tags: ITag[];
+}
+
+interface IForm2 {
+  name: string;
+  tags: ITag[];
+}
+
+@model("myApp/Tag")
+export class Tag extends Model({ label: prop<string>("").withSetter() }) {}
+@model("myApp/Form1")
+export class Form1 extends Model({
+  tags: prop<Tag[]>(() => []).withSetter(),
+  name: prop<string>("").withSetter(),
+}) {}
+
+@model("myApp/Form2")
+export class Form2 extends Model({
+  tags: prop<Tag[]>(() => []).withSetter(),
+  name: prop<string>("").withSetter(),
+}) {}
+
+export namespace SettingsUtils {
+  /**
+   * pathFromChild
+   * @param child
+   * @returns list of path segments for the given child under Settings
+   */
+  export const pathFromChild = (child: any) => {
+    const res = findParentPath(
+      child,
+      (parent) => parent instanceof Settings,
+      5 // how deep?
+    );
+    return res?.path;
+  };
+
+  /**
+   *
+   * @param path list of path segments
+   * @param instance settings instance to resolve the path on
+   * @returns found child instance / undefined
+   */
+  export const childFromPath = (path: Path | undefined, instance: Settings) => {
+    if (!path) {
+      return;
+    }
+    return resolvePath(instance, path);
+  };
+}
+
+@model("myApp/Settings")
+export class Settings extends Model({
+  name: prop<string>("").withSetter(),
+  form1List: prop<Form1[]>(() => []).withSetter(),
+  form2List: prop<Form2[]>(() => []).withSetter(),
+  tags: prop<Tag[]>(() => []),
+}) {
+  @modelAction arrayRemoveAt(array: any[], index: number) {
+    array.splice(index, 1);
+  }
+
+  @modelAction arrayPush(array: any[], item: any) {
+    array.push(item);
+  }
+
+  @modelAction addForm1() {
+    this.form1List.push(new Form1({}));
+  }
+
+  @modelAction addForm2() {
+    this.form2List.push(new Form2({}));
+  }
+}
+
+type Path = ReadonlyArray<string | number>;
+
+@model("myApp/Root")
+export class Root extends Model({
+  settings: prop<Settings | undefined>(),
+  selectionPath: prop<Path | undefined>(),
+}) {
+  @observable.ref settingsDraft: Draft<Settings> | undefined;
+  @observable.ref selectedChild: any;
+  onInit() {
+    if (this.settings) {
+      this.setSettings(this.settings);
+    }
+  }
+  @modelAction setSettings(val: Settings) {
+    this.settings = val;
+    this.settingsDraft = draft(this.settings);
+    this.setChildFromSelectionPath();
+  }
+  @modelAction commit() {
+    this.settingsDraft?.commit();
+  }
+
+  @modelAction setSelectionPath(child: any) {
+    this.selectedChild = child;
+    this.selectionPath = SettingsUtils.pathFromChild(child);
+  }
+
+  @modelAction setChildFromSelectionPath() {
+    if (!this.selectionPath) {
+      return;
+    }
+    const res = SettingsUtils.childFromPath(
+      this.selectionPath,
+      this.settingsDraft?.data!
+    );
+    if (res?.resolved) {
+      this.selectedChild = res.value;
+    } else {
+      this.selectedChild = undefined;
+    }
+  }
+}
